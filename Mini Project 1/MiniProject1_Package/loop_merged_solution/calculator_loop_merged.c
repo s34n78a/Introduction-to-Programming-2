e5ee05f223048efb5f5574d07d0e6b8ae1c1465e
s34n781 <patrick.sean781@gmail.com>
2026-05-22 16:15:53 +0800

    Optimized Solution

--- Mini Project 1/MiniProject1_Package/loop_merged_solution/calculator_loop_merged.c
+++ Mini Project 1/MiniProject1_Package/loop_merged_solution/calculator_loop_merged.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <stdarg.h> // TO DO: header file for variable argument functions (va_list, va_start, va_end, etc.)
 
 // for lex
 #define MAXLEN 256
